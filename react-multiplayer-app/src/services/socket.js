40fbdfb3daee7041984c7d4550b4abd6eb0dc822
Bradston Henry <bradston.henry@gmail.com>
2021-09-10 14:37:02 -0400

    Socket server update for testing

--- react-multiplayer-app/src/services/socket.js
+++ react-multiplayer-app/src/services/socket.js
@@ -1,5 +1,5 @@
 import socketIOClient from "socket.io-client";
-const serverEndpoint = "http://localhost:8080/";
+const serverEndpoint = "http://multiplayer-game-server-rhos-a-2d-multiplayer.mult-server-bsh-b3c-4x16-162e406f043e20da9b0ef0731954a894-0000.us-east.containers.appdomain.cloud/";
 
 export const socket = socketIOClient(serverEndpoint, {
 	transports: ['websocket']
